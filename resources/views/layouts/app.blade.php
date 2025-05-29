16907099dd9b8e7fe863e5e426c8ba464d812174
Zachary Gallant <zachgallant26@gmail.com>
2025-05-29 11:28:07 -0300

    changed small fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,25 +59,17 @@
     @else
         <nav class="navbar navbar-expand-lg bg-body-tertiary px-3 py-2">
             <div class="container-fluid">
-                @hasanyrole('admin|developer')
-                <a class="navbar-brand" href="{{ route('index') }}">Support Ticket System</a>
-                @else
-                <a class="navbar-brand" href="{{ route('unauthorized') }}">Support Ticket System</a>
-                @endhasanyrole
+                <a class="navbar-brand" href="{{ route('home') }}">Support Ticket System</a>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         @auth
                             @hasanyrole('admin|developer')
-                                <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="{{ route('index') }}">Homes</a>
-                                </li>   
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('users-roles') }}">Assign Roles</a>
-                                </li> 
-                            @else
-                                <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="{{ route('unauthorized') }}">Home</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" aria-current="page" href="{{ route('home') }}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('users-roles') }}">Assign Roles</a>
+                            </li>
                             @endhasanyrole
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('user-logout') }}">Logout</a>
