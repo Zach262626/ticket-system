dc9fbfa53ae797c5698a95a8c0940aa01b13b0ea
Zachary Gallant <zachgallant26@gmail.com>
2025-06-02 11:27:27 -0300

    wrap fixo on table

--- resources/views/components/ticket/list-table.blade.php
+++ resources/views/components/ticket/list-table.blade.php
@@ -1,52 +1,52 @@
 <div>
-    <table class="table mt-2 w-100">
+    <table class="table mt-2 w-100 table-fixed align-middle text-break">
         <thead>
             <tr>
                 <th class="col-1" scope="col">Ticket #</th>
                 <th class="col-5" scope="col">Description</th>
                 <th class="col-2" scope="col">Type</th>
                 @can('delete tickets')
-                <th class="col-1" scope="col">Status</th>
-                <th class="col-1 text-center" scope="col">Delete</th>
+                    <th class="col-1" scope="col">Status</th>
+                    <th class="col-1 text-center" scope="col">Delete</th>
                 @else
-                <th class="col-2" scope="col">Status</th>
+                    <th class="col-2" scope="col">Status</th>
                 @endcan
                 @can('edit tickets')
-                <th class="col-1 text-center" scope="col">Edit</th>
-                <th class="col-1 text-center" scope="col">View</th>
+                    <th class="col-1 text-center" scope="col">Edit</th>
+                    <th class="col-1 text-center" scope="col">View</th>
                 @else
-                <th class="col-2 text-center" scope="col">View</th>
+                    <th class="col-2 text-center" scope="col">View</th>
                 @endcan
             </tr>
         </thead>
         <tbody>
             @foreach ($tickets as $ticket)
-            <tr>
-                <td scope="row">{{ $ticket->id }}</td>
-                <td>{{ $ticket->description }}</td>
-                <td>{{ optional($ticket->type)->name }}</td>
-                @can('delete tickets')
-                <td>{{ optional($ticket->status)->name }}</td>
-                <td>
-                    <button type="button" class="btn btn-danger w-100" data-bs-toggle="modal"
-                        data-bs-target="#confirmDeleteModal">
-                        Delete
-                    </button>
-                    <x-ticket.modal.delete :ticket=$ticket />
-                </td>
-                @else
-                <td>{{ optional($ticket->status)->name }}</td>
-                @endcan
-                @can('edit tickets')
-                <td><a class="btn btn-primary w-100"
-                        href="{{ route('ticket-edit', ['ticket' => $ticket->id]) }}">Edit</a></td>
-                <td><a class="btn btn-secondary w-100"
-                        href="{{ route('ticket-show', ['ticket' => $ticket->id]) }}">Here</a></td>
-                @else
-                <td><a class="btn btn-secondary w-100"
-                        href="{{ route('ticket-show', ['ticket' => $ticket->id]) }}">Here</a></td>
-                @endcan
-            </tr>
+                <tr>
+                    <td scope="row">{{ $ticket->id }}</td>
+                    <td>{{ $ticket->description }}</td>
+                    <td>{{ optional($ticket->type)->name }}</td>
+                    @can('delete tickets')
+                        <td>{{ optional($ticket->status)->name }}</td>
+                        <td>
+                            <button type="button" class="btn btn-danger w-100" data-bs-toggle="modal"
+                                data-bs-target="#confirmDeleteModal">
+                                Delete
+                            </button>
+                            <x-ticket.modal.delete :ticket=$ticket />
+                        </td>
+                    @else
+                        <td>{{ optional($ticket->status)->name }}</td>
+                    @endcan
+                    @can('edit tickets')
+                        <td><a class="btn btn-primary w-100"
+                                href="{{ route('ticket-edit', ['ticket' => $ticket->id]) }}">Edit</a></td>
+                        <td><a class="btn btn-secondary w-100"
+                                href="{{ route('ticket-show', ['ticket' => $ticket->id]) }}">Here</a></td>
+                    @else
+                        <td><a class="btn btn-secondary w-100"
+                                href="{{ route('ticket-show', ['ticket' => $ticket->id]) }}">Here</a></td>
+                    @endcan
+                </tr>
             @endforeach
         </tbody>
     </table>
