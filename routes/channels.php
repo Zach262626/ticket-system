88e0dfc8907aa85e04aef126502af75e42bd6940
Zachary Gallant <zachgallant26@gmail.com>
2025-06-16 12:51:53 -0300

    small fix in channel

--- routes/channels.php
+++ routes/channels.php
@@ -12,9 +12,12 @@ use Stancl\Tenancy\Middleware\ScopeSessions;
 
 
 
-
-Broadcast::channel('tenant-{tenantId}.user-{userId}', function ($user, $tenantId, $userId) {
-    return tenant()->id == (int) $tenantId && $user->id === (int) $userId;
+Broadcast::channel('tenant-{tenantId}.ticket-{ticketId}', function ($user, $tenantId, $ticketId) {
+    return ($tenantId == tenant()->id) && Ticket::where('id', $ticketId)
+        ->where(function ($query) use ($user) {
+            $query->where('created_by', $user->id)
+                ->orWhere('accepted_by', $user->id);
+        })->exists();
 });
 
 Broadcast::channel('tenant-{tenantId}.user-{userId}', function ($user, $tenantId, $userId) {
